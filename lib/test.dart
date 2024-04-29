379f8efa09da59eb1ea53d1b34782e5466d5345c
Ho Young Jhoo <starvessel@naver.com>
2024-04-29 19:44:27 +0900

    todo app copy

--- lib/test.dart
+++
@@ -1,8 +0,0 @@
-void test() {
-  var t = "aser";
-  var l = t.codeUnits;
-  for (final i in l) {
-    // code point to charactoer
-    print(String.fromCharCode(i));
-  }
-}
---
+++ lib/todo.dart
@@ -0,0 +1,74 @@
+import 'package:riverpod/riverpod.dart';
+import 'package:uuid/uuid.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'todo.freezed.dart';
+// part 'todo.g.dart';
+
+const _uuid = Uuid();
+
+@freezed
+class Todo with _$Todo {
+  const factory Todo({
+    required String id,
+    required String description,
+    @Default(false) bool completed,
+  }) = _Todo;
+
+  @override
+  String toString() {
+    return 'Todo(description: $description, completed: $completed)';
+  }
+}
+
+/// An object that controls a list of [Todo].
+class TodoList extends Notifier<List<Todo>> {
+  @override
+  List<Todo> build() => [
+        const Todo(id: 'todo-0', description: 'Buy cookies'),
+        const Todo(id: 'todo-1', description: 'Star Riverpod'),
+        const Todo(id: 'todo-2', description: 'Have a walk'),
+      ];
+
+  void add(String description) {
+    state = [
+      ...state,
+      Todo(
+        id: _uuid.v4(),
+        description: description,
+      ),
+    ];
+  }
+
+  void toggle(String id) {
+    state = [
+      for (final todo in state)
+        if (todo.id == id)
+          Todo(
+            id: todo.id,
+            completed: !todo.completed,
+            description: todo.description,
+          )
+        else
+          todo,
+    ];
+  }
+
+  void edit({required String id, required String description}) {
+    state = [
+      for (final todo in state)
+        if (todo.id == id)
+          Todo(
+            id: todo.id,
+            completed: todo.completed,
+            description: description,
+          )
+        else
+          todo,
+    ];
+  }
+
+  void remove(Todo target) {
+    state = state.where((todo) => todo.id != target.id).toList();
+  }
+}
